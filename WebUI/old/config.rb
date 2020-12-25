9a9812c7afca1bbe671aa9e12d84655dce153d3f
FoolHen <fulgen_3_7ad@hotmail.com>
2020-12-25 11:43:42 +0100

    refactor: remove old UI files

--- WebUI/old/config.rb
+++
@@ -1,9 +0,0 @@
-http_path = "/"
-css_dir = "assets/css"
-sass_dir = "assets/scss"
-images_dir = "assets/img"
-fonts_dir = "assets/fonts"
-javascripts_dir = "app"
-relative_assets = false
-
-output_style = :compressed #:expanded or :nested or :compact or :compressed
